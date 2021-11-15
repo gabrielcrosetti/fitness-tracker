298c1f66e6d08c430968fc80ef5b2cdd4abb3d64
Gabriel Crosetti <crosetti.gabriel@gmail.com>
2021-11-15 12:10:37 -0500

    Changes to exercises model

--- Develop/models/exercises.js
+++ Develop/models/exercises.js
@@ -2,3 +2,15 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema
 
+const ExerciseSchema = new Schema({
+    name: String,
+    type: String,
+    weight: Number,
+    sets: Number,
+    reps: Number,
+    duration: Number,
+    distance: Number
+})
+
+const Exercise = mongoose.model("exercises", ExerciseSchema);
+module.exports = Exercise; 
